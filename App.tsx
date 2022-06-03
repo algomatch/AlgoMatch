e5f33c665bb2d24e7440ac206b7c2d4a2e3d46e5
TX Ho <tingxianho@gmail.com>
2022-06-03 02:10:23 +0100

    Application complete

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { Amplify } from 'aws-amplify';
 import config from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
@@ -10,88 +10,50 @@ import db from './utils/algos.json';
 import logo from './assets/algomatch-logo.png';
 
 function App() {
+	const algos = db.algos;
 	const [lastSwipe, setLastSwipe] = useState('');
-	// const onSwipe = (direction: any) => {
-	// 	console.log(`You swiped ${direction}.`);
-	// };
+	const [swipeCount, setSwipeCount] = useState(0);
+	const [imageRendered, setImageRendered] = useState(false);
+	const [index, setIndex] = useState(-1);
+	const [viewChat, setViewChat] = useState(false);
+
+	const pictures = [
+		{ id: '1', name: 'David', uri: require('./assets/dave.jpeg') },
+		{ id: '2', name: 'Scott', uri: require('./assets/scott.png') },
+		{ id: '3', name: 'Kevin', uri: require('./assets/kev.png') },
+		{ id: '4', name: 'TX', uri: require('./assets/tx.png') },
+		{ id: '5', name: 'Vince', uri: require('./assets/vince.jpeg') },
+	];
 
 	const swiped = (direction: any, nameToDelete: string) => {
+		if (direction === 'right') setSwipeCount((p) => ++p);
 		setLastSwipe(`You swiped ${direction} on ${nameToDelete}.`);
 	};
 
-	const Container = styled.View`
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-	`;
+	const openChat = () => {
+		setViewChat(true);
+	};
 
-	const Header = styled.Text`
-		color: #000;
-		font-size: 30px;
-		margin-bottom: 30px;
-	`;
+	useEffect(() => {
+		console.log(`Swipe count: ${swipeCount}`);
+		if (swipeCount % 3 === 2) {
+			setImageRendered(true);
+			setIndex((p) => ++p);
+		} else {
+			setImageRendered(false);
+		}
+	}, [swipeCount]);
 
-	const CardContainer = styled.View`
-		width: 90%;
-		max-width: 260px;
-		height: 300px;
-		margin-top: 50%;
-	`;
+	useEffect(() => {
+		console.log(`Index: ${index}`);
+	}, [index]);
 
-	const Card = styled.View`
-		position: absolute;
-		background-color: #fff;
-		width: 100%;
-		max-width: 260px;
-		height: 400px;
-		shadow-color: black;
-		shadow-opacity: 0.2;
-		shadow-radius: 2px;
-		border-radius: 20px;
-		resize-mode: cover;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	`;
-
-	const CardImage = styled.ImageBackground`
-		display: flex;
-		justify-content: center;
-		align-item: center;
-	`;
-
-	const CardTitle = styled.Text`
-		position: absolute;
-		bottom: 0;
-		margin: 10px;
-		color: #fff;
-	`;
-
-	const Buttons = styled.View`
-		margin: 20px;
-		z-index: -100;
-	`;
-
-	const InfoText = styled.Text`
-		height: 28px;
-		justify-content: center;
-		display: flex;
-		z-index: -100;
-	`;
-
-	const CardText = styled.Text`
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-top: 150px;
-	`;
+	const incrementSwipeCount = () => {};
 
 	return (
 		<Container>
-			<CardImage source={logo}></CardImage>
 			<CardContainer>
-				{db.algos.map((algorithm) => (
+				{algos.map((algorithm) => (
 					<TinderCard
 						className='swipe'
 						key={algorithm.name}
@@ -100,12 +62,25 @@ function App() {
 						// style={styles1.card}
 					>
 						<Card>
-							<Text>{algorithm.name}</Text>
+							{imageRendered ? (
+								<CardImage source={pictures[index].uri}></CardImage>
+							) : (
+								// <CardImage source={pictures[0].uri}></CardImage>
+								<Text>{algorithm.name}</Text>
+							)}
 						</Card>
 					</TinderCard>
 				))}
 			</CardContainer>
-			<CardText>{lastSwipe}</CardText>
+
+			{imageRendered ? (
+				<>
+					<CardText>You matched with {pictures[index].name}!</CardText>
+					<Button title='Open chat' onPress={openChat}></Button>
+				</>
+			) : (
+				<CardText>{lastSwipe} </CardText>
+			)}
 		</Container>
 	);
 }
@@ -117,5 +92,74 @@ const authenticatorOptions = {
 		hiddenDefaults: ['phone_number'],
 	},
 };
+
+const Container = styled.View`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+`;
+
+// const Header = styled.Text`
+// 	color: #000;
+// 	font-size: 30px;
+// 	margin-bottom: 30px;
+// `;
+
+const CardContainer = styled.View`
+	width: 90%;
+	max-width: 260px;
+	height: 300px;
+	margin-top: 50%;
+`;
+
+const Card = styled.View`
+	position: absolute;
+	background-color: #fff;
+	width: 100%;
+	max-width: 260px;
+	height: 400px;
+	shadow-color: black;
+	shadow-opacity: 0.2;
+	shadow-radius: 2px;
+	border-radius: 20px;
+	resize-mode: cover;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const CardImage = styled.ImageBackground`
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	border-radius: 20px;
+`;
+
+// const CardTitle = styled.Text`
+// 	position: absolute;
+// 	bottom: 0;
+// 	margin: 10px;
+// 	color: #fff;
+// `;
+
+const Buttons = styled.View`
+	margin: 20px;
+	z-index: -100;
+`;
+
+const InfoText = styled.Text`
+	height: 28px;
+	justify-content: center;
+	display: flex;
+	z-index: -100;
+`;
+
+const CardText = styled.Text`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 150px;
+`;
 
 export default withAuthenticator(App, authenticatorOptions);
--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,3 @@
 declare module 'aws-amplify-react-native';
 declare module 'styled-components';
+declare module './assets/algomatch-logo.png';
